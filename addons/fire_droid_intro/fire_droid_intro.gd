b9ae0d08c9e497cb935819e4670bc545d66794c7
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-08-08 09:38:13 -0300

    feat: Add FDIntro custom node to Fire Droid Intro plugin
    Introduces a new custom node type 'FDIntro' by registering it in the EditorPlugin. Adds the corresponding script and UID file for the node, enabling its use within the Godot editor.

--- addons/fire_droid_intro/fire_droid_intro.gd
+++ addons/fire_droid_intro/fire_droid_intro.gd
@@ -3,8 +3,13 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	pass
+	add_custom_type(
+		"FDIntro",
+		"Node",
+		preload("res://addons/fire_droid_intro/scripts/fd_intro.gd"),
+		preload("res://addons/fire_droid_intro/icons/FDIntro.svg")
+	)
 
 
 func _exit_tree() -> void:
-	pass
+	remove_custom_type("FDIntro")
---
+++ addons/fire_droid_intro/scripts/fd_intro.gd
@@ -0,0 +1,16 @@
+@tool
+extends Node
+class_name FDIntro
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
