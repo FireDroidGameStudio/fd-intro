09da841aca1b29dd4b0cd56a3daa8d0492c39fbf
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-08-08 12:48:04 -0300

    feat: Add frame animation generation to FDIntro
    Introduces export variables for SpriteFrames and FPS, and updates the intro animation generation to create keyframes based on the provided frames and FPS. Refactors node name handling for AnimationPlayer and TextureRect to support dynamic creation and retrieval.

--- addons/fire_droid_intro/scripts/fd_intro.gd
+++ addons/fire_droid_intro/scripts/fd_intro.gd
@@ -3,11 +3,20 @@ extends Node
 class_name FDIntro
 
 
+@export var frames: SpriteFrames = null
+@export_range(0.01, 60.0, 0.01, "or_greater") var fps: float = 30.0
 @export_tool_button("Generate Intro", "AnimationMixer")
 var _generate_intro_button: Callable = _generate_intro
 
-@onready var _animation_player: AnimationPlayer = get_node_or_null("AnimationPlayer")
-@onready var _texture_rect: TextureRect = get_node_or_null("TextureRect")
+var _animation_player_node_name: String = "AnimationPlayer"
+var _texture_rect_node_name: String = "TextureRect"
+
+@onready var _animation_player: AnimationPlayer = (
+	get_node_or_null(_animation_player_node_name)
+)
+@onready var _texture_rect: TextureRect = (
+	get_node_or_null(_texture_rect_node_name)
+)
 
 
 func _ready() -> void:
@@ -29,13 +38,15 @@ func generate_intro() -> void:
 func _generate_intro() -> void:
 	if _animation_player == null:
 		_animation_player = AnimationPlayer.new()
-		add_child(_animation_player, true)
+		add_child(_animation_player, true)#, Node.INTERNAL_MODE_FRONT)
+		_animation_player_node_name = _animation_player.name
 		_animation_player.set_owner(self)
 	if _texture_rect == null:
 		_texture_rect = TextureRect.new()
-		add_child(_texture_rect, true)
-		_texture_rect.set_owner(self)
+		add_child(_texture_rect, true)#, Node.INTERNAL_MODE_BACK)
+		_texture_rect_node_name = _texture_rect.name
 		_texture_rect.call_deferred(&"set_anchors_preset", Control.PRESET_FULL_RECT)
+		_texture_rect.set_owner(self)
 	var animation_library: AnimationLibrary = (
 		_animation_player.get_animation_library(&"FDIntro")
 	)
@@ -51,5 +62,15 @@ func _generate_intro() -> void:
 
 func _update_default_animation(animation: Animation) -> void:
 	animation.clear()
+	var frame_count: int = frames.get_frame_count(&"default")
+	var total_duration: float = 0.0
+	var frame_duration: float = 0.0
+	if frame_count > 0:
+		total_duration = float(frame_count) / fps
+		frame_duration = total_duration / float(frame_count)
 	var track_index: int = animation.add_track(Animation.TYPE_VALUE)
 	animation.track_set_path(track_index, _texture_rect.name + ":texture")
+	animation.length = total_duration
+	for i: int in frame_count:
+		var frame: Texture2D = frames.get_frame_texture(&"default", i)
+		animation.track_insert_key(track_index, i * frame_duration, frame)
